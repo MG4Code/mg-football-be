05026c34dc3efa06c6f59f7506300a0e26421536
Milad Khakpour <milad.khakpour@gmail.com>
2020-01-08 16:08:46 +0100

    get player by id and add crossOrigin for Frontend

--- src/main/java/com/mg/backend/player/PlayerResource.java
+++ src/main/java/com/mg/backend/player/PlayerResource.java
@@ -9,11 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/player")
 public class PlayerResource {
 
@@ -23,6 +26,11 @@ public class PlayerResource {
   @GetMapping
   public List<Player> list() {
     return service.list();
+  }
+
+  @GetMapping("id")
+  public Optional<Player> findPlayer(@RequestParam("q") String id) {
+    return service.findPlayerById(id);
   }
 
   @GetMapping("club")
