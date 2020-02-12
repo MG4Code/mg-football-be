a176a8f98a0bb8d40f243122329a8a13b1e818ad
georg <georg@noenv.com>
2020-02-12 22:39:13 +0100

    add oauth2 google security,currenty only authentication no authorization

--- src/main/java/com/mg/backend/BackendApplication.java
+++ src/main/java/com/mg/backend/BackendApplication.java
@@ -1,15 +1,13 @@
 package com.mg.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 
 }
