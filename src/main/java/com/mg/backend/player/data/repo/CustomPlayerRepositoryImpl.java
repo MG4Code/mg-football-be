b3bf22e19c6e8b03e6e6ecc76fb808bfdbcd1c04
basalt79 <aigner.georg.79@gmail.com>
2020-02-16 20:45:31 +0100

    cannot find a RxJavaMongoTemplate, so i use ReactiveMongoTemplate and transform it afterwards

--- src/main/java/com/mg/backend/player/data/repo/CustomPlayerRepositoryImpl.java
+++ src/main/java/com/mg/backend/player/data/repo/CustomPlayerRepositoryImpl.java
@@ -4,27 +4,27 @@ import com.mg.backend.player.data.dto.Player;
 import com.mg.backend.player.data.dto.Position;
 import io.reactivex.Maybe;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Component;
+import reactor.adapter.rxjava.RxJava2Adapter;
 
+@Component
 public class CustomPlayerRepositoryImpl implements CustomPlayerRepository {
 
-  private final MongoTemplate mongoTemplate;
+  private final ReactiveMongoTemplate mongoTemplate;
 
   @Autowired
-  public CustomPlayerRepositoryImpl(MongoTemplate mongoTemplate) {
+  public CustomPlayerRepositoryImpl(ReactiveMongoTemplate mongoTemplate) {
     this.mongoTemplate = mongoTemplate;
   }
 
   @Override
   public Maybe<Player> getFirstGoalKeeper() {
     var query = new Query(Criteria.where("position").is(Position.GOAL_KEEPER));
-    var players = mongoTemplate.find(query, Player.class);
-    if (!players.isEmpty()) {
-      return Maybe.just(players.get(0));
-    } else {
-      return Maybe.empty();
-    }
+    return RxJava2Adapter.fluxToFlowable(mongoTemplate.find(query, Player.class))
+      .firstElement();
+
   }
 }
